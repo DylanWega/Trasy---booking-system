44e99bbc139b71be2cbabd22384e8dcb69a3f56a
DylanWega <P2670683@my365.dmu.ac.uk>
2023-04-19 20:47:23 +0100

    -Working login page

--- app/src/main/java/com/example/trasy/MainActivity.java
+++ app/src/main/java/com/example/trasy/MainActivity.java
@@ -2,12 +2,15 @@ package com.example.trasy;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    DatabaseHelper databaseHelper;
 
     EditText email, password;
     Button signup, login;
@@ -20,7 +23,7 @@ public class MainActivity extends AppCompatActivity {
         password = (EditText) findViewById(R.id.txtPassword);
         signup = (Button) findViewById(R.id.signupBtn);
         login = (Button) findViewById(R.id.loginBtn);
-
+        DatabaseHelper DB = new DatabaseHelper(this);
         signup.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -31,7 +34,23 @@ public class MainActivity extends AppCompatActivity {
         login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                String em = email.getText().toString();
+                String pwd = password.getText().toString();
+                // check if fields are empty and throws msg if empty
+                if(em.equals("") || pwd.equals(""))
+                    Toast.makeText(MainActivity.this, "Please enter all fields",Toast.LENGTH_SHORT).show();
+                else{
+                    Boolean checkUser = DB.checkEmailnPassword(em,pwd);
+                    if(checkUser == true){
+                        Toast.makeText(MainActivity.this, "Sign in Successfull", Toast.LENGTH_SHORT).show();
+                        Intent intent = new Intent(getApplicationContext(), Homepage.class);
+                        startActivity(intent);
+                    }
+                    else{
+                        Toast.makeText(MainActivity.this,"Invalid Credentials", Toast.LENGTH_SHORT).show();
+                        System.out.println("Invalid Credentials");
+                    }
+                }
             }
         });
     }
