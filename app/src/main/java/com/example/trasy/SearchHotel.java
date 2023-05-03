0cb815ed1d67c67b075dc836d1c34f344ac7879e
jamachiudokporo <ujamachi@gmail.com>
2023-05-03 20:42:09 +0100

    added new resource file for recycler view

--- app/src/main/java/com/example/trasy/SearchHotel.java
+++ app/src/main/java/com/example/trasy/SearchHotel.java
@@ -8,9 +8,12 @@ public class SearchHotel extends AppCompatActivity {
     // fields that will be displayed on search
     private String hotelName;
     private String hotelLocation;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search_hotel);
+
+
     }
 }
