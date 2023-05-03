04677366135e40dd10c6cfd21588c212bd733f96
jamachiudokporo <ujamachi@gmail.com>
2023-05-03 19:39:36 +0100

    added search hotel attributes

--- app/src/main/java/com/example/trasy/SearchHotel.java
+++ app/src/main/java/com/example/trasy/SearchHotel.java
@@ -5,7 +5,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class SearchHotel extends AppCompatActivity {
-
+    // fields that will be displayed on search
+    private String hotelName;
+    private String hotelLocation;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
