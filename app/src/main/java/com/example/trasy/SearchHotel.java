674b37136e2ca79c8e71a0e3baba5fcca3e9b01d
jamachiudokporo <ujamachi@gmail.com>
2023-05-12 06:35:07 +0100

    added "if" condition to account for empty fields

--- app/src/main/java/com/example/trasy/SearchHotel.java
+++ app/src/main/java/com/example/trasy/SearchHotel.java
@@ -78,68 +78,75 @@ public class SearchHotel extends AppCompatActivity {
                 String numberOfRooms = roomAmt.getText().toString();
                 String numberOfChildren = childrenNo.getText().toString();
 
-                // field for OkHttpClient
-                OkHttpClient client = new OkHttpClient();
+                // if the fields are not empty, continue as intended
+                if (!location.equals("") || !checkIn.equals("") || !checkOut.equals("") || !numberOfAdults.equals("") || !numberOfRooms.equals("") || !numberOfChildren.equals("")) {
+                    // field for OkHttpClient
+                    OkHttpClient client = new OkHttpClient();
 
-                Request request = new Request.Builder()
-                        .url("https://airbnb13.p.rapidapi.com/search-location?location=" + location + "&checkin=2023-09-16&checkout=2023-09-17&adults="+ numberOfAdults +"&children=" + numberOfChildren +"&infants=0&pets=0&page=1&currency=USD")
-                        .get()
-                        .addHeader("Accept", "*/*")
-                        .addHeader("User-Agent", "Thunder Client (https://www.thunderclient.com)")
-                        .addHeader("X-RapidAPI-Key", "12efc02e68msh8a95a40a10aabf5p104c4bjsn49d4264a561d")
-                        .addHeader("X-RapidAPI-Host", "airbnb13.p.rapidapi.com")
-                        .build();
+                    Request request = new Request.Builder()
+                            .url("https://airbnb13.p.rapidapi.com/search-location?location=" + location + "&checkin=2023-09-16&checkout=2023-09-17&adults=" + numberOfAdults + "&children=" + numberOfChildren + "&infants=0&pets=0&page=1&currency=USD")
+                            .get()
+                            .addHeader("Accept", "*/*")
+                            .addHeader("User-Agent", "Thunder Client (https://www.thunderclient.com)")
+                            .addHeader("X-RapidAPI-Key", "12efc02e68msh8a95a40a10aabf5p104c4bjsn49d4264a561d")
+                            .addHeader("X-RapidAPI-Host", "airbnb13.p.rapidapi.com")
+                            .build();
 
-                client.newCall(request).enqueue(new Callback() {
-                    @Override
-                    public void onFailure(@NonNull Call call, @NonNull IOException e) {
-                        e.printStackTrace();
-                    }
-
-                    @Override
-                    public void onResponse(@NonNull Call call, @NonNull Response response) throws IOException {
-                        // field to hold the json format of the response
-                        JSONObject hotelJsonResponse;
-                        // will not work without try/catch
-                        if (response.body() != null) {
-                            try {
-
-                                hotelJsonResponse = new JSONObject(response.body().string());
-                                System.out.println(hotelJsonResponse);
-                                aHotel = new Hotel();
-                                JSONArray results = hotelJsonResponse.getJSONArray("results");
-
-                                for (int i = 0; i < results.length(); i++){
-                                    Hotel aHotel2 = new Hotel();
-
-                                    JSONObject firstHotelName = results.getJSONObject(i);
-                                    String hotelName = firstHotelName.getString("name");
-                                    JSONObject firstHotelPrice = results.getJSONObject(i).getJSONObject("price");
-                                    String price = firstHotelPrice.getString("rate");
-                                    aHotel2.setHotelName(hotelName);
-                                    aHotel2.setPrice(price);
-                                    HotelList.add(aHotel2);
-                                }
-
-                            } catch (JSONException e) {
-                                AlertDialog.Builder builder = new AlertDialog.Builder(SearchHotel.this);
-                                throw new RuntimeException(e);
-                            }
-                            // display the view on the main UI thread
-                            // otherwise throws a CalledFromWrongThreadException
-                            runOnUiThread(new Runnable() {
-                                @Override
-                                public void run() {
-                                    HotelList.add(aHotel);
-                                    viewAdapter.notifyDataSetChanged();
-                                }
-                            });
-                        } else {
-                            // Handle unsuccessful response
-                            System.out.println("Unsuccessful response: " + response.code());
+                    client.newCall(request).enqueue(new Callback() {
+                        @Override
+                        public void onFailure(@NonNull Call call, @NonNull IOException e) {
+                            e.printStackTrace();
                         }
-                    }
-                });
+
+                        @Override
+                        public void onResponse(@NonNull Call call, @NonNull Response response) throws IOException {
+                            // field to hold the json format of the response
+                            JSONObject hotelJsonResponse;
+                            // will not work without try/catch
+                            if (response.body() != null) {
+                                try {
+
+                                    hotelJsonResponse = new JSONObject(response.body().string());
+                                    System.out.println(hotelJsonResponse);
+                                    aHotel = new Hotel();
+                                    JSONArray results = hotelJsonResponse.getJSONArray("results");
+
+                                    for (int i = 0; i < results.length(); i++) {
+                                        Hotel aHotel2 = new Hotel();
+
+                                        JSONObject firstHotelName = results.getJSONObject(i);
+                                        String hotelName = firstHotelName.getString("name");
+                                        JSONObject firstHotelPrice = results.getJSONObject(i).getJSONObject("price");
+                                        String price = firstHotelPrice.getString("rate");
+                                        aHotel2.setHotelName(hotelName);
+                                        aHotel2.setPrice(price);
+                                        HotelList.add(aHotel2);
+                                    }
+
+                                } catch (JSONException e) {
+                                    AlertDialog.Builder builder = new AlertDialog.Builder(SearchHotel.this);
+                                    throw new RuntimeException(e);
+                                }
+                                // display the view on the main UI thread
+                                // otherwise throws a CalledFromWrongThreadException
+                                runOnUiThread(new Runnable() {
+                                    @Override
+                                    public void run() {
+                                        HotelList.add(aHotel);
+                                        viewAdapter.notifyDataSetChanged();
+                                    }
+                                });
+                            } else {
+                                // Handle unsuccessful response
+                                System.out.println("Unsuccessful response: " + response.code());
+                            }
+                        }
+                    });
+                }
+                // else show "fill all fields" message
+                else {
+                    Toast.makeText(getApplicationContext(), "Please fill in the fields", Toast.LENGTH_SHORT).show();
+                }
             }
         });
     }
